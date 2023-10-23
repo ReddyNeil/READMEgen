916cdadfa1e36670d6a5813b42f8cf9512881f36
ReddyNeil <141553827+ReddyNeil@users.noreply.github.com>
2023-10-23 22:36:09 +1100

    added markdown function and updated commentary

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,82 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to render badge image for licence type
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
+}
+// Function to create a link to licence section
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return (
+      `\n* [License](#license)\n`
+    );
+  }
+  return '';
+}
+// Function to create the licence section
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return (
+      `## License
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+This project is licensed under the ${license} license.`
+    );
+  }
+  return '';
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// Function to create Markdown for README 
 function generateMarkdown(data) {
   return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+${renderLicenseLink(data.license)}
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, please contact me directly at ${data.email}.
 
 `;
 }
 
 module.exports = generateMarkdown;
+
